b99c9b8592991d359ef2903e6f6b2fa38fd99060
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2022-03-07 21:53:38 +0530

    done d1 U3

--- Unit3/orderingTheprocess.js
+++ Unit3/orderingTheprocess.js
@@ -1,6 +1,27 @@
 function runProgram(input) {
     // Write code here
+    input = input.trim().split("\n");
+    let N =+input[0];
+    let arr1 =input[1].trim().split(" ").map(Number);
+    let arr2 =input[2].trim().split(" ").map(Number);
+    order(arr1,arr2,N);
    
+  }
+  function order(arr1,arr2,N){
+      let i =0;
+      let count =0;
+     
+      while(arr1.length>0){
+          if(arr1[i]!=arr2[i]){
+arr1.push(arr1.shift());
+count++;
+          }else{
+              arr1.shift();
+              arr2.shift();
+              count++;
+          }
+      }
+      console.log(count);
   }
   if (process.env.USER=== "anmolkumar") {
     runProgram(`3
