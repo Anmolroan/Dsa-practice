5b819e431d6f19229517d9e9ab831ae1d8735751
Anmolroan <anmol.kumar@masaischool.com>
2022-04-26 01:21:27 +0530

    linked link practice

--- Unit4/InsertAtHead.js
+++ Unit4/InsertAtHead.js
@@ -1,15 +1,44 @@
-
-const LinkedListNode = class {
-    constructor(nodeData) {
-        this.data = nodeData;
-        this.next = null;
+const LinkedListNode =class{
+    constructor(item){
+        this.data =item;
+        this.next=null
     }
-};
-
-// Complete the function below
-
-function insertNodeAtHead(head, data) {
-     let newNode = new LinkedListNode(data);
-    newNode.next = head;
-    return newNode
 }
+const node1 =new LinkedListNode(4)
+
+function add(head,data){
+    let node2 = new LinkedListNode(data)
+    if(head===null){
+        head =node1
+        return head;
+    }else{
+        node2.next=head;
+        head =node2;
+        return head;
+    }
+}
+add(node1,3);
+function print(head){
+    cur =head;
+    while(cur!=null){
+        console.log(cur.data);
+        cur =cur.next
+    }
+}
+
+function InsertAtEnd(head,data){
+     const node1=new LinkedListNode(data);
+    
+        cur =head;
+
+        while(cur!=null){
+            cur=cur.next;
+        }
+        // console.log(cur)
+        cur=node1;
+        // console.log(cur)
+        
+    
+}
+InsertAtEnd(node1,60)
+print(node1);
