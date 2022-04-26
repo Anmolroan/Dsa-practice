5393e60b8aaa149d2431e9eff80888bc122f28ee
Anmolroan <anmol.kumar@masaischool.com>
2022-04-26 19:10:06 +0530

    linked list practice

--- Unit4/InsertAtHead.js
+++ Unit4/InsertAtHead.js
@@ -4,41 +4,77 @@ const LinkedListNode =class{
         this.next=null
     }
 }
-const node1 =new LinkedListNode(4)
 
-function add(head,data){
-    let node2 = new LinkedListNode(data)
-    if(head===null){
-        head =node1
-        return head;
-    }else{
-        node2.next=head;
-        head =node2;
-        return head;
-    }
-}
-add(node1,3);
-function print(head){
-    cur =head;
-    while(cur!=null){
-        console.log(cur.data);
-        cur =cur.next
+const Node  =class {
+    constructor(item){
+        this.data=item;
+        this.next =null;
     }
 }
 
-function InsertAtEnd(head,data){
-     const node1=new LinkedListNode(data);
+const firstNode =new Node(4)
+
+const secondNode =new Node(5)
+firstNode.next = secondNode;
+
+console.log(firstNode.next);
+// console.log(secondNode);
+
+function addAtZero(head,data){
+const ThirdNode =new Node(data);
+ThirdNode.next = head;
+head=ThirdNode;
+console.log(head);
+}
+
+addAtZero(firstNode,6)
+
+function AddAtTail(head,data){
+var FourthNode =new Node(data);
+let cur =head;
+while(cur.next!==null){
+    cur=cur.next;
+}
+cur.next=FourthNode;
+console.log(head);
+}
+
+AddAtTail(firstNode,7)
+// const node1 =new LinkedListNode(4)
+
+// function add(head,data){
+//     let node2 = new LinkedListNode(data)
+//     if(head===null){
+//         head =node1
+//         return head;
+//     }else{
+//         node2.next=head;
+//         head =node2;
+//         return head;
+//     }
+// }
+// add(node1,3);
+// function print(head){
+//     cur =head;
+//     while(cur!=null){
+//         console.log(cur.data);
+//         cur =cur.next
+//     }
+// }
+
+// function InsertAtEnd(head,data){
+//      const node1=new LinkedListNode(data);
     
-        cur =head;
+//         cur =head;
 
-        while(cur!=null){
-            cur=cur.next;
-        }
-        // console.log(cur)
-        cur=node1;
-        // console.log(cur)
+//         while(cur!=null){
+//             cur=cur.next;
+//         }
+//         // console.log(cur)
+//         cur=node1;
+//         // console.log(cur)
         
     
-}
-InsertAtEnd(node1,60)
-print(node1);
+// }
+// InsertAtEnd(node1,60)
+// print(node1);
