dcfb1ed5fe54bb5b5cf8fbd073db47813d02609b
Anmolroan <anmol.kumar@masaischool.com>
2022-05-16 23:08:10 +0530

    dsa

--- DSA-421/CheckForSymmetry.js
+++ DSA-421/CheckForSymmetry.js
@@ -13,7 +13,48 @@ matrix.push(input[line++].trim());
    }
   }
   function main(matrix) {
-      
+      let flag1  =true;
+      let N = matrix.length
+      let i =0;
+      let j =N-1;
+      while(i<j){
+          if(matrix[i]===matrix[j]){
+              i++;
+              j--;
+          }else{
+            flag1 = false;
+            break;
+          }
+      }
+      let flag2 =true;
+      let arr =[];
+      for(let i=0;i<N;i++){
+          let str ="";
+          for(let j =0;j<N;j++){
+            str=str+matrix[j][i]
+          }
+          arr.push(str)
+      }
+     let l=0;
+     let r =arr.length-1;
+     while(l<r){
+         if(arr[l]===arr[r]){
+             l++;
+             r--;
+         }else{
+             flag2=false;
+             break;
+         }
+     }
+if(flag1&&flag2){
+    console.log("BOTH")
+}else if(flag1){
+    console.log("HORIZONTAL")
+}else if(flag2){
+    console.log("VERTICAL")
+}else{
+    console.log("NO")
+}
   }
   if (process.env.USER=== "anmolkumar") {
     runProgram(`3
