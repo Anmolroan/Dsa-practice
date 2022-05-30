e157a8a49518ae790090cc29d1e48404beca149f
Anmolroan <anmol.kumar@masaischool.com>
2022-05-30 14:56:11 +0530

    numbers game

---
+++ DSA-421/MasaiNumbersGame.js
@@ -0,0 +1,65 @@
+function runProgram(input) {
+    // Write code here
+    input=input.trim().split("\n");
+    let line =1;
+ let N =+input[0];
+ let arr=[];
+ for(let i =0; i<N;i++){
+    arr.push(+input[line++]);
+ }  
+ main(arr,N);
+  }
+  function main(arr,N) {
+      let stack = [];
+      let top=-1;
+      let ans = [];
+      let s =[];
+      let t=-1;;
+      for(let i =N-1;i>=0;i--){
+      
+        while(stack.length>0&&stack[top]<=arr[i]){
+            stack.pop();
+            top--;
+        }if(stack.length>0){
+         
+            
+          
+
+        }else{
+           
+            ans.push(-1)
+        }
+       
+        stack.push(arr[i]);
+        top++;
+      }
+      console.log(ans);
+  }
+  if (process.env.USER=== "anmolkumar") {
+    runProgram(`8
+    3
+    7
+    1
+    7
+    8
+    4
+    5
+    2`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0) ;
+    });
+  }
