8166cba581cb4ce9a87b04e49d9ab2e805cd1c2a
Anmolroan <anmol.kumar@masaischool.com>
2022-06-24 14:28:17 +0530

    diff of k

---
+++ Ankushquestions/Revision/3.3.2.1/DiffrenceOfK.js
@@ -0,0 +1,37 @@
+function runProgram(input) {
+    // Write code here
+    input=input.trim().split("\n");
+    var test=+input[0];
+    var line=1;
+    for(let i = 0;i<test;i++){
+      var [N,K]=input[line++].trim().split(" ").map(Number)
+      var arr=input[line++].trim().split(" ").map(Number);
+      find(N,K,arr)
+    }
+   
+  }
+  if (process.env.USER=== "anmolkumar") {
+    runProgram(`2
+    5 3
+    1 2 3 4 5
+    5 8
+    1 2 3 4 5 `);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0) ;
+    });
+  }
+  
