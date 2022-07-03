a7acf0d59c907c7059ddde691ce77a79e9c3f67f
Anmolroan <anmol.kumar@masaischool.com>
2022-07-03 20:19:28 +0530

    solved adam and EVE

--- placements/AdamAndEve.js
+++ placements/AdamAndEve.js
@@ -1,65 +1,53 @@
-// function runProgram(input) {
-//     // Write code here
-//     input=input.trim().split("\n");
-//     let tc =+input[0];
-//     let line =1;
-//     for(let i =0;i<tc;i++){
-//         let N =+input[line++];
-//      let arr=input[line++].trim().split("");
-//      main(arr,N);
-//     }
-//   }
-//   function main(arr,N){
-//     let count = 0;
-//     let eve=3
-//     let dp=[]
-//    let rec=(cur,newarr)=>{
-//     if(dp[cur]){
-//         return dp[cur];
-//     }
-//     if(newarr.length===eve){
-     
-//       if(newarr.join("")==="EVE"){
-//         count++;
-//       }
-        
-//     }
-//     if(cur===N){
-//         return;
-//     }
-//     for(let i=cur;i<arr.length;i++){
-//         newarr.push(arr[i]);
-//       dp[i]=rec(i+1,newarr);
-//         newarr.pop();
-//     }
-//    }
-//    rec(0,[]);
-//    console.log(count)
-//   }
-//   if (process.env.USER=== "anmolkumar") {
-//     runProgram(`2
-//     3
-//     EVE
-//     4
-//     EVEE`);
-//   } else {
-//     process.stdin.resume();
-//     process.stdin.setEncoding("ascii");
-//     let read = "";
-//     process.stdin.on("data", function (input) {
-//       read += input;
-//     });
-//     process.stdin.on("end", function () {
-//       read = read.replace(/\n$/, "");
-//       read = read.replace(/\n$/, "");
-//       runProgram(read);
-//     });
-//     process.on("SIGINT", function () {
-//       read = read.replace(/\n$/, "");
-//       runProgram(read);
-//       process.exit(0) ;
-//     });
-//   }
+function runProgram(input) {
+    // Write code here
+    input=input.trim().split("\n");
+    let tc =+input[0];
+    let line =1;
+    for(let i =0;i<tc;i++){
+        let N =+input[line++];
+     let arr=input[line++].trim().split("");
+     main(arr,N);
+    }
+  }
+  function main(arr,N){
+    let e=0;
+    let v=0;
+    let ebv=0;
+    let count=0;
+for(let i = 0;i<arr.length;i++){
+if(arr[i]==="E"){
+    count=count+ebv;
+    e++;
+}else if(arr[i]==="V"){
+  
+    ebv=ebv+e;
+    
+}
 
-let a= [1,2,3,4];
-console.log(typeof a)
+}  
+console.log(count)
+
+  }
+  if (process.env.USER=== "anmolkumar") {
+    runProgram(`1
+    4
+    EVVVEE`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0) ;
+    });
+  }
+
