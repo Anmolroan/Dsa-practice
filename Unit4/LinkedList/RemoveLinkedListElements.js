0b1f3b91799b1c294d405c0cb7775a511672f3df
Anmolroan <anmol.kumar@masaischool.com>
2022-07-11 18:42:30 +0530

    not done

--- Unit4/LinkedList/RemoveLinkedListElements.js
+++ Unit4/LinkedList/RemoveLinkedListElements.js
@@ -4,7 +4,7 @@ const LinkedListNode = class {
         this.next = null;
     }
 };
-let arr =[1,2,6,3,4,5,6];
+let arr =[1,2,6,3,4,5,7];
 let head = new LinkedListNode(arr[0]);
 let i =1;
 let cur=head;
@@ -14,3 +14,28 @@ let temp =new LinkedListNode(arr[i]);
     cur=cur.next;
     i++;
 };
+var removeElements = function(head, val) {
+    let cur =head;
+    if(cur.val===val){
+        cur=cur.next;
+        head=cur;
+    }
+   let prev=cur;
+    while(cur.next!=null){
+        prev=cur
+        cur=cur.next;
+        if(cur.val===val){
+           prev.next=cur.next;
+            
+        }
+         
+        
+        
+    }
+    // if(cur.val===val){
+    //     cur=cur.next;
+    
+    // }
+    return head
+};
+console.log(removeElements(head,6))
