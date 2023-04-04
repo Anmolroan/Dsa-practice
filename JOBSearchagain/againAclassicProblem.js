af4c42e75161d6ecba556dcbca3ef22b9dcbcd62
Anmolroan <baloochat0@gmail.com>
2023-04-04 22:57:38 +0530

    solved the again a classic problem using stack

--- JOBSearchagain/againAclassicProblem.js
+++ JOBSearchagain/againAclassicProblem.js
@@ -9,24 +9,41 @@ function runProgram(input) {
    
   }
   function main(str){
-    console.log(str);
-    let i =0;
-    let j =str.length-1;
-    let flag=false;
-    while(i<j){
-        if(str[i]==="("&&str[j]==")" ||str[i]==="{"&&str[j]=="}" ||str[i]==="["&&str[j]=="]"){
-            flag=true;
-            i++;
-            j--;
+    // console.log(str);
+    // let i =0;
+    // let j =str.length-1;
+    // let flag=false;
+    // while(i<j){
+    //     if(str[i]==="("&&str[j]==")" ||str[i]==="{"&&str[j]=="}" ||str[i]==="["&&str[j]=="]"){
+    //         flag=true;
+    //         i++;
+    //         j--;
+    //     }else{
+    //         flag=false;
+    //         break;
+    //     }
+    // }
+    // if(flag){
+    //     console.log("balanced")
+    // }else{
+    //     console.log("unbalanced")
+    // }
+    let stack =[];
+    let top=-1;
+    for(let i =0;i<str.length;i++){
+        if(stack.lenght!=0 &&stack[top]+str[i]==="()" || stack[top]+str[i]==="{}" ||stack[top]+str[i]==="[]"){
+            stack.pop();
+            top--;
         }else{
-            flag=false;
-            break;
+            stack.push(str[i]);
+            top++;
         }
+        
     }
-    if(flag){
-        console.log("balanced")
+    if(stack.length>0){
+        console.log("not balanced");
     }else{
-        console.log("unbalanced")
+        console.log("balanced");
     }
 
 
@@ -37,8 +54,7 @@ function runProgram(input) {
     runProgram(`3
     {([])}
     ()
-    ([]
-    `);
+    ([]`);
   } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
